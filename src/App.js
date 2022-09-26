187062102a36cdd81baa341efa7605f4304359a0
hamidaKa <galaihamida@gmail.com>
2022-09-26 22:15:20 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import img from "./hm.jpg"
+export default class App extends Component {
+  state = {
+   person: {name: "hamida",
+    bio:" ",
+    imgsrc:img,
+    profession:"ingenieur informatique"},
+    show:"false",
+    mountingTime : null,
+    timeSnceMounting:null
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  };
+  handleClick=(e)=>{
+    e.preventDefault();
+    this.setState({show:!this.state.show})
+
+    
+  }
+  componentDidMount() {
+    this.setState({mountingTime : new Date()})
+
+   setInterval(() => {
+    const date = new Date();
+    this.setState((prevstate) => ({
+      timeSnceMounting : Math.floor((date- prevstate.mountingTime)/1000 )
+    }));
+    
+   }, 1000); 
+  }
+ 
+  render() {
+    return (
+      <div>
+
+        <button onClick={this.handleClick} >Show</button>
+        
+ 
+       {this.state.show? 
+
+       <div className='container'>
+          <div  className='Name'>
+            <h1>hello {this.state.person.name}</h1>
+          </div>
+          <div  className='img'>
+           <img style= {{width:"150px", height:"150px" }} src ={this.state.person.imgsrc}></img> 
+          </div>
+          <div  className='bio'>
+            {this.state.person.bio}
+          </div>
+        <div  className='profession'>
+          {this.state.person.profession}
+          </div> 
+        <div  className='mounting'>
+        <h3>Time since mounting: {this.state.timeSnceMounting}</h3></div> 
+        </div> : null}
+      </div>
+       )
+  }
 }
 
-export default App;
+
+
